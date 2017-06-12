321f84c65010b2fc552e785214ed06fe5bfff67f
Matthias Elliott <komizutama@gmail.com>
2017-06-12 12:02:13 -0400

    added in roadmap of project

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,3 +1,15 @@
 defmodule Identicon do
-@modu
+@moduledoc """
+  A module that will  create an identicon based on the name of the user. it will display vertical symmetry and and produces the same identicon for the same name.
+"""
+
+# String
+
+# Compute MD5 hash of the string
+# List of numbers based on the string
+# Pick a color
+# Build a grid of squares
+# Convert grid into image
+# Save identicon to disk as an image
+
 end
