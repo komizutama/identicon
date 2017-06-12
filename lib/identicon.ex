7179f056e56eb2a9d80f25eecf87c724827b6399
Matthias Elliott <komizutama@gmail.com>
2017-06-12 12:11:54 -0400

    takes input of string hashes and provides numbvers

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,11 +2,22 @@ defmodule Identicon do
 @moduledoc """
   A module that will  create an identicon based on the name of the user. it will display vertical symmetry and and produces the same identicon for the same name.
 """
-
+# 
 # String
+@doc """
+  This function is the main 
+"""
+def main(input) do
+  input
+  |> hash_input
+end
 
-# Compute MD5 hash of the string
-# List of numbers based on the string
+# Compute MD5 hash of the string & Generate a list of numbers based on the string
+def hash_input(input) do
+  :crypto.hash(:md5, input)
+  |> :binary.bin_to_list
+end
+
 # Pick a color
 # Build a grid of squares
 # Convert grid into image
