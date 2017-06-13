46b4779bb5049b7fa131826bbbf61dbc360b0dee
Matthias Elliott <komizutama@gmail.com>
2017-06-13 15:05:15 -0400

    completed

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -12,6 +12,10 @@ def main(input) do
   |> hash_input
   |> pick_color
   |> build_grid
+  |> filter_odd_squares
+  |> build_pixel_map
+  |> draw_image
+  |> save_image(input)
 end
 
 @doc """
@@ -35,11 +39,15 @@ end
 Grid of square will consist of a five by five pattern read from L2R  3 number packets will be used to determine value. The first number => first square, second number=> second square third # => middle square value and then it mirrors. If the number is even then it will be  a colored square, if it is odd it will be white.  
 """
 def build_grid(%Identicon.Image{hex: hex} = image) do
-  hex
-  |> Enum.chunk(3) #break into chunks of three
-  #mirror_rows and put into a new list
-  |> Enum.map(&mirror_row/1) #&mirror_row/1 tells it to pass everything to mirror_row with an arrity of 1
-  |> List.flatten
+  grid = 
+    hex
+    |> Enum.chunk(3) #break into chunks of three
+    #mirror_rows and put into a new list
+    |> Enum.map(&mirror_row/1) #&mirror_row/1 tells it to pass everything to mirror_row with an arrity of 1
+    |> List.flatten
+    |> Enum.with_index
+
+  %Identicon.Image{image | grid: grid}
 end
 
 @doc """
@@ -50,9 +58,55 @@ def mirror_row(row) do
     [first, second | _tail] = row
     #[145, 46, 200, 46, 145]
     row ++ [second, first]
+end
+
+@doc """
+  Filters out the odd numbers and updates the struct
+"""
+def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+  grid = Enum.filter grid, fn({code, _index}) -> 
+    rem(code, 2) == 0
   end
 
-# Convert grid into image
-# Save identicon to disk as an image
+  %Identicon.Image{image | grid: grid}#return updated version of grid in image struct
+end
 
+@doc """
+ This builds a set of tuples. That will act as the defining grid points for the image
+ """
+def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+  #generate top left and bottom right points for each colored square
+  pixel_map = Enum.map grid, fn({ _code, index}) ->
+    horizontal = rem(index, 5) * 50
+    vertical = div(index, 5) * 50
+
+    top_left = {horizontal, vertical}
+    bottom_right = {horizontal + 50, vertical + 50}
+
+    {top_left, bottom_right}
+  end
+
+  %Identicon.Image{image | pixel_map: pixel_map}
+end
+
+@doc """
+  Draws the actual image.
+"""
+def draw_image (%Identicon.Image{color: color, pixel_map: pixel_map}) do
+  image = :egd.create(250, 250) #creates a 250px square blank canvas
+  fill = :egd.color(color) #creates a color variable for use later
+
+  #creates the filled rectangles
+  Enum.each pixel_map, fn({start,stop}) ->
+    :egd.filledRectangle(image, start, stop, fill)
+  end
+
+  #renders the image
+  :egd.render(image)
+end
+
+# Save identicon to disk as an image
+  def save_image(image, input) do
+  File.write("#{input}.png", image)
+  end
 end
--- lib/image.ex
+++ lib/image.ex
@@ -2,5 +2,5 @@ defmodule Identicon.Image do
   @moduledoc """
   This module creates the image storage structure
   """
-  defstruct hex: nil, color: nil
+  defstruct hex: nil, color: nil, grid: nil, pixel_map: nil
 end
